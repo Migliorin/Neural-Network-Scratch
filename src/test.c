cc906b0a09f5cb629e783ee574d3ca9d8079b40a
Lucas Migliorin da Rosa <lucasmigliorin@hotmail.com>
2026-02-07 09:17:46 -0400

    feat: New tests and fix it

--- src/test.c
+++ src/test.c
@@ -6,6 +6,7 @@ int main(){
 	struct Matrix *mat;
 	struct Matrix *mat2;
 	struct Matrix *trans;
+	struct Matrix *mul;
 
 	mat = createMat(3,4);
 	mat2 = createMat(4,3);
@@ -13,23 +14,36 @@ int main(){
 	fillMatrixRandomNumber(mat);
 	fillMatrixRandomNumber(mat2);
 
+	printf("Matrix 3x4\n");
 	printMatrix(mat);
 	printf("\n");
+
+	printf("Matrix 4x3\n");
 	printMatrix(mat2);
+	printf("\n");
+
 
 	mulMatScalar(mat,-2.0);
 
-	printf("\n");
+	printf("Matrix 3x4 multiplicado por (-2)\n");
 	printMatrix(mat);
+	printf("\n");
 
 	trans = transpose(mat);
 
-	printf("\n");
+	printf("Matrix 3x4 transposta\n");
 	printMatrix(trans);
+	printf("\n");
+
+	printf("Multiplicacao matrix 3x4 pela matrix 4x3\n");
+	mul = mulMat(mat, mat2);
+    	printMatrix(mul);
+	printf("\n");
 
 
 	freeMatrix(mat);
 	freeMatrix(mat2);
 	freeMatrix(trans);
+	freeMatrix(mul);
 	return 0;
 }
